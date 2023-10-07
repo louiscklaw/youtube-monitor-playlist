c12f6b28d0a74dd41dbcb80a9ff350c77662d552
Louis Law <louiscklaw@users.noreply.github.com>
2023-10-07 15:29:26 +0800

    update youtube-monitor-playlist,

--- browserless-helloworld/puppeteer-helloworld.js
+++ browserless-helloworld/puppeteer-helloworld.js
@@ -5,17 +5,19 @@ const puppeteer = require('puppeteer');
   var browser;
   var output = { state: 'init', debug: {}, error: {} };
   try {
-    // If your script executes too quickly, you can add a ?pause query parameter
-    // to the connect call to pause the script from running until you're watching it
     browser = await puppeteer.connect({ browserWSEndpoint: `ws://localhost:3000` });
     const page = await browser.newPage();
 
     // Full puppeteer API is available
     await page.goto('https://louiscklaw.github.io/');
     await page.screenshot({ path: './screens/01-main-screen.png' });
+
+    output = { ...output, state: 'done' };
   } catch (error) {
     console.log(error);
+    output = { ...output, state: 'error', error };
   }
 
+  console.log({ output });
   browser && browser.close();
 })();
