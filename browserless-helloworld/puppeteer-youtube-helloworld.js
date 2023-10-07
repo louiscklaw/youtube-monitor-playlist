5e9aa07be4d3c5c0ac1d82204fac56e119da7705
Louis Law <louiscklaw@users.noreply.github.com>
2023-10-07 16:34:55 +0800

    update youtube-monitor-playlist,

--- browserless-helloworld/puppeteer-youtube-helloworld.js
+++ browserless-helloworld/puppeteer-youtube-helloworld.js
@@ -5,12 +5,22 @@ const puppeteer = require('puppeteer-extra');
   var browser;
   var output = { state: 'init', debug: {}, error: {} };
   try {
-    browser = await puppeteer.connect({ browserWSEndpoint: `ws://localhost:3000` });
+    browser = await puppeteer.connect({ browserWSEndpoint: `ws://localhost:3000?--window-size=1920,1080` });
     const page = await browser.newPage();
 
     // Full puppeteer API is available
-    await page.goto('https://www.youtube.com/@01official/videos');
-    await page.screenshot({ path: './screens/01-youtube-helloworld.png' });
+    // https://www.youtube.com/@SingTaoHeadline/videos
+    await page.goto('https://www.youtube.com/@01official/videos', { waitUntil: 'networkidle2' });
+    await page.setViewport({ width: 1920, height: 1080 * 5 });
+    await page.screenshot({ path: './screens/01-hk01-helloworld.png' });
+
+    await page.goto('https://www.youtube.com/@ChannelCHK/videos', { waitUntil: 'networkidle2' });
+    await page.setViewport({ width: 1920, height: 1080 * 5 });
+    await page.screenshot({ path: './screens/01-channel_c-helloworld.png' });
+
+    await page.goto('https://www.youtube.com/@SingTaoHeadline/videos', { waitUntil: 'networkidle2' });
+    await page.setViewport({ width: 1920, height: 1080 * 5 });
+    await page.screenshot({ path: './screens/01-sing-tao-headline-helloworld.png' });
 
     output = { ...output, state: 'done' };
   } catch (error) {
